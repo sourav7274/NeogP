7ab120d46e7664bc53d4fe08f3ffdb41b16986ff
Sourav Sarkar <samjhonson932@gmail.com>
2024-06-05 21:55:12 +0530

    FP2.4_HW2

--- CW/script.js
+++ CW/script.js
@@ -1,26 +1,26 @@
 const show = document.getElementById("show")
 const state = document.getElementsByName("status")
-const tasks = [
+const items = [
 
-    {id: 1, task: "Task 1", status: "todo"},
+    { id: 1, item: "Item 1", status: "In Sale" },
   
-    {id: 2, task: "Task 2", status: "completed"},
+    { id: 2, item: "Item 2", status: "New Release" },
   
-    {id: 3, task: "Task 3", status: "inProgress"},
+    { id: 3, item: "Item 3", status: "New Release" },
   
-    {id: 4, task: "Task 4", status: "todo"},
+    { id: 4, item: "Item 4", status: "In Sale" },
   
-    {id: 5, task: "Task 5", status: "completed"}
+    { id: 5, item: "Item 5", status: "In Sale" }
   
   ]
 
   function render(val)
   {
-     const list = val === "all" ? tasks : tasks.filter(task => task.status==val)
+     const list = val === "all" ? items : items.filter(item => item.status==val)
      const stuff = list.map(list => `
      <li>
      <b>ID:</b>${list.id}<br>
-     <b>Task:</b>${list.task}<br>
+     <b>Item:</b>${list.item}<br>
      <b>Status:</b>${list.status}<br>
      </li>
      <hr>     
