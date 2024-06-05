8b1c61798b54ee68cb3b1c1cfd7c4a3c32e4ab45
Sourav Sarkar <samjhonson932@gmail.com>
2024-06-05 20:40:21 +0530

    FP2.4_CW

--- CW/script.js
+++ CW/script.js
@@ -1,28 +1,30 @@
 const show = document.getElementById("show")
 
 const data = [
-    {id:1,name:"Alice",exp:2},
-    {id:2,name:"Bob",exp:9},
-    {id:3,name:"Charlie",exp:4},
-    {id:4,name:"John",exp:19}
+   {id:1,title:"Movie 1",genre:"action"},
+   {id:2,title:"Movie 2",genre:"comedy"},
+   {id:3,title:"Movie 3",genre:"drama"},
+   {id:4,title:"Movie 4",genre:"action"},
+   {id:5,title:"Movie 5",genre:"comedy"},
 ]
 
-const expI = document.getElementById("expI")
+const radioB = document.getElementsByName('genre')
 function render(val)
 {
-    const list = val ? data.filter(emp => emp.exp>=5) : data
-    const content= list.map(person => `
-    <li>
-    <b>ID:</b>${person.id}<br>
-    <b>Name:</b>${person.name}<br>
-    <b>Experience:</b>${person.exp} Years<br>
+    const list = val === "all" ? data : data.filter(d => d.genre==val)
+    const movieHtml = list.map(d => `
+<li>
+    <b>ID:</b>${d.id}<br>
+    <b>Title:</b>${d.title}<br>
+    <b>Genre:</b>${d.genre}<br>
     <hr>
-    </li>
-    `)
-
-    show.innerHTML = content.join(" ")
+</li>
+`)
+show.innerHTML = movieHtml.join("")
 }
-render(false)
-expI.addEventListener("change",function(){
-    render(expI.checked)
+render("all")
+radioB.forEach(b => {
+    b.addEventListener("change",function(){
+        render(this.value)
+    })
 })
