d51c9539818d5572a299059b23c7e0759e7892c5
Sourav Sarkar <samjhonson932@gmail.com>
2024-06-13 19:28:19 +0530

    IR1.5_HW2

--- CW/test1/src/App.js
+++ CW/test1/src/App.js
@@ -1,95 +1,59 @@
 
 import './App.css';
 
-const ArrCal = () => {
-  const numbers = [1, 2, 3, 4, 5];
-  const sum = numbers.reduce((acc,curr) =>{
-    acc+=curr
-    return acc
-  } ,0)
-  const avg = sum/numbers.length
-  const max = numbers.reduce((acc,curr) =>{
-    if(curr>acc)
+const Rnum = () =>{
+  let a = Math.floor(Math.random()*100)+1
+  return <p>Random number between 1 and 100 is {a}</p>
+}
+
+const AbsVal = () =>{
+  const number = -10
+  return <p>Absolute Value: {Math.abs(number)}</p>
+}
+
+const SumE = () => {
+  const numbers = [1, 2, 3, 4, 5, 6, 7]
+  const sum = numbers.filter(n => {
+    let sum = 0
+    if(n%2===0)
       {
-        acc=curr
+        sum+=n
       }
-    return acc
-  },numbers[0])
-  const min = numbers.reduce((acc,curr) =>{
-    if(curr<acc)
-      {
-        acc=curr
-      }
-    return acc
-  },numbers[0])
+    return sum
+  })
+  return <p>Sum of Even Numbers: {sum}</p>
+}
+
+const Psq = () => {
+  const side = 5
   return(
     <div>
-        <h1>Array Opeartions</h1>
-        <p>Sum:{sum}</p>
-        <p>Avg:{avg}</p>
-        <p>Max:{max}</p>
-        <p>Min:{min}</p>
-    </div>
-  )
-}
-const person = {
-
-  name: "Sarah",
-
-  age: 28,
-
-  city: "New York",
-
-};
-const ObjectManipulation = ({person}) =>{
-    person.age=29
-    return(
-      <div>
-        <h1>Person Detail</h1>
-        <p>Name: {person.name}</p>
-        <p>Age: {person.age}</p>
-        <p>City: {person.city}</p>
-      </div>
-    )
-}
-
-const SquareArea= () => {
-  const sides = 5
-  return (
-    <div>
-      <h1>Area of Square</h1>
-      <p>The area of a square with sides {sides} is {sides*sides}</p>
+      <h1>Square Perimeter Calculator</h1>
+      <p>Side Length: {side}cm</p>
+      <p>Perimeter: {side*4}cm</p>
     </div>
   )
 }
 
-const Capitalist = () => {
-  const word = "hello world"
+const Convert =() =>{
+  const amount = 50
   return(
     <div>
-      <h1>Capitalize Text</h1>
-      <p>{word.toUpperCase()}</p>
-    </div>
-  )
-}
-
-const AreaC = () => {
-  const r = 8
-  return(
-    <div>
-      <h1>Area of Circle</h1>
-      <p>The area of circle is: {3.14*r*r}</p>
+      <h1>Currency Converter</h1>
+      <p>Amount in USD:${amount} </p>
+      <p>Exchnage Rate: 1USD = 74.5INR </p>
+      <p>Amount in INR: INR {amount*74.5}</p>
     </div>
   )
 }
 function App() {
   return (
     <div>
-      <ArrCal />
-      <ObjectManipulation person={person} />
-      <SquareArea />
-      <Capitalist />
-      <AreaC />
+      <Rnum />
+      <AbsVal/>
+      <SumE />
+      <Psq />
+      <Convert />
     </div>
   );
 }
