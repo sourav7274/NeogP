a04fbf267126639a1656ac36594269f24c136797
Sourav Sarkar <samjhonson932@gmail.com>
2024-06-13 16:59:56 +0530

    IR1.4_CW

--- CW/test1/src/App.js
+++ CW/test1/src/App.js
@@ -1,91 +1,57 @@
 
 import './App.css';
 
-const Greetings = (props) => {
-  return <h1>Welcome to {props.name} app!</h1>
+const employee = {
+  id:1,
+  name:"John Doe",
+  position:"Software Developer",
+  department:"Engineering",
+  email:"johndoe@gmail.com"
 }
 
-const ReciepeDetail =(props) => {
-  return (
-    <div>
-    <h1>Recipe Details</h1>
-      <p>Title: {props.title}</p>
-      <p>Author: {props.author}</p>
-      <p>Genre: {props.rating}</p>
-    </div>
-  )
-}
-const recipeData = {
-
-  title: "Chocolate Cake",
-
-  author: "Baker Betty",
-
-  rating: 4.7,
-
-};
-
-
-const MovieInformation = (props) => {
+const EmployeeDetails = ({employee}) =>{
   return(
     <div>
-      <h1>Movie Information</h1>
-      <p>Title: {props.title} C </p>
-      <p>Author: {props.director} </p>
-      <p>Release Date: {props.releaseDate}</p>
+      <h2>Employee Details</h2>
+      <p>Name: {employee.name}</p>
+      <p>position: {employee.position}</p>
+      <p>Department: {employee.department} </p>
+      <p>Email: {employee.email}</p>
     </div>
   )
 }
 
-const ProductFeatures = (props) => {
+const Footer = () =>{
   return(
-    <div>
-      <h1>Product Details</h1>
-      <p>Name: {props.name} </p>
-      <p>Color: {props.color} </p>
-      <p>Size: {props.size} </p>
-    </div>
+    <footer>
+      <p>&copy; 2024 Company Name. All rights reserved</p>
+    </footer>
   )
 }
 
-const UserProfile = (props) => {
+const Header = () =>{
   return(
-    <div>
-      <h1>User Details</h1>
-      <p>Name: {props.name}</p>
-      <p>Email: {props.email} </p>
-      <p>Role: {props.role} </p>
-    </div>
+    <header>
+       <h1>Welcome to our Company</h1>
+       <nav>
+        <ul>
+          <li>
+            <a href='/'>Name</a>
+          </li>
+        </ul>
+       </nav>
+    </header>
+   
   )
 }
 
-const movieData = {
-
-  title: "The Godfather",
-
-  director: "Francis Ford Coppola",
-
-  releaseDate: "March 24, 1972",
-
-};
-const productData = {
-
-  name: "Smartwatch",
-
-  color: "Black",
-
-  size: "Medium",
-
-};
 function App() {
   return (
-    <div>
-       <Greetings name="My Reciepe"/>
-       <ReciepeDetail title={recipeData.title} author={recipeData.author} rating = {recipeData.rating} />
-       <MovieInformation title={movieData.title} director={movieData.director} releaseDate ={movieData.releaseDate} />
-       <ProductFeatures name={productData.name} color = {productData.color} size = {productData.size} />
-       <UserProfile name="Alici" email= "Emaili.com" role="Roli" />  
-    </div>
+    <di>
+    <Header/>
+      <EmployeeDetails employee={employee} />
+      <Footer/>
+    </di>
   );
 }
 
