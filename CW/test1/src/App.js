4d11eeaba7a451b98275886874a4febfa0b2584c
Sourav Sarkar <samjhonson932@gmail.com>
2024-06-13 13:08:18 +0530

    IR1.3_HW2

--- CW/test1/src/App.js
+++ CW/test1/src/App.js
@@ -2,49 +2,48 @@
 import './App.css';
 
 const Greetings = (props) => {
-  return <h1>Welcome,{props.name}</h1>
+  return <h1>Welcome to {props.name} app!</h1>
 }
 
-const BookDetail =(props) => {
+const ReciepeDetail =(props) => {
   return (
     <div>
-    <h1>Book Details</h1>
+    <h1>Recipe Details</h1>
       <p>Title: {props.title}</p>
       <p>Author: {props.author}</p>
-      <p>Genre: {props.genre}</p>
+      <p>Genre: {props.rating}</p>
     </div>
   )
 }
+const recipeData = {
 
-const ArticleInformation = (props) => {
+  title: "Chocolate Cake",
+
+  author: "Baker Betty",
+
+  rating: 4.7,
+
+};
+
+
+const MovieInformation = (props) => {
   return(
     <div>
-      <h1>Artilce Information</h1>
+      <h1>Movie Information</h1>
       <p>Title: {props.title} C </p>
-      <p>Author: {props.author} </p>
-      <p>Published Data: {props.publishedDate}</p>
+      <p>Author: {props.director} </p>
+      <p>Release Date: {props.releaseDate}</p>
     </div>
   )
 }
 
-const ProductDetails = (props) => {
+const ProductFeatures = (props) => {
   return(
     <div>
       <h1>Product Details</h1>
       <p>Name: {props.name} </p>
-      <p>Brand: {props.brand} </p>
-      <p>Price: {props.price} </p>
-    </div>
-  )
-}
-
-const EventDetails = (props) => {
-  return(
-    <div>
-      <h1>Event Details</h1>
-      <p>Title: {props.title}</p>
-      <p>Location: {props.location} </p>
-      <p>Date: {props.date}  </p>
+      <p>Color: {props.color} </p>
+      <p>Size: {props.size} </p>
     </div>
   )
 }
@@ -52,57 +51,40 @@ const EventDetails = (props) => {
 const UserProfile = (props) => {
   return(
     <div>
-      <h1>User Profile</h1>
+      <h1>User Details</h1>
       <p>Name: {props.name}</p>
-      <img src = {props.url} alt = {props.alt} />
+      <p>Email: {props.email} </p>
+      <p>Role: {props.role} </p>
     </div>
   )
 }
-const eventData = {
 
-  title: "Tech Conference 2024",
+const movieData = {
 
-  location: "San Francisco",
+  title: "The Godfather",
 
-  date: "April 15, 2024",
+  director: "Francis Ford Coppola",
+
+  releaseDate: "March 24, 1972",
 
 };
 const productData = {
 
-  name: "Smartphone",
+  name: "Smartwatch",
 
-  brand: "Samsung",
+  color: "Black",
 
-  price: 799.99,
-
-};
-const bookData = {
-
-  title: "To Kill a Mockingbird",
-
-  author: "Harper Lee",
-
-  genre: "Classic",
-
-};
-const articleData = {
-
-  title: "The Power of Habit",
-
-  author: "Charles Duhigg",
-
-  publishedDate: "March 1, 2024",
+  size: "Medium",
 
 };
 function App() {
   return (
     <div>
-       <Greetings name="Emily"/>
-       <BookDetail title={bookData.title} author={bookData.author} genre = {bookData.genre} />
-       <ArticleInformation title={articleData.title} author={articleData.author} publishedDate ={articleData.publishedDate} />
-       <ProductDetails name={productData.name} brand = {productData.brand} price = {productData.price} />
-       <EventDetails title ={eventData.title} location = {eventData.location} date = {eventData.date}  />
-       <UserProfile name="Chirs" url= "https://placehold.co/150x150?text=Hello+World" alt="Unga Bunga"/>
+       <Greetings name="My Reciepe"/>
+       <ReciepeDetail title={recipeData.title} author={recipeData.author} rating = {recipeData.rating} />
+       <MovieInformation title={movieData.title} director={movieData.director} releaseDate ={movieData.releaseDate} />
+       <ProductFeatures name={productData.name} color = {productData.color} size = {productData.size} />
+       <UserProfile name="Alici" email= "Emaili.com" role="Roli" />  
     </div>
   );
 }
