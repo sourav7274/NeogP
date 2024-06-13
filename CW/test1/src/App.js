1d7c93aa3dd8007c7d992ed86bff35c841903e16
Sourav Sarkar <samjhonson932@gmail.com>
2024-06-13 12:54:54 +0530

    IR1.3_HW1

--- CW/test1/src/App.js
+++ CW/test1/src/App.js
@@ -2,46 +2,107 @@
 import './App.css';
 
 const Greetings = (props) => {
-  return <h1>Hello,{props.name}</h1>
+  return <h1>Welcome,{props.name}</h1>
 }
 
-const UserDetail =(props) => {
+const BookDetail =(props) => {
   return (
     <div>
-    <h1>User Details</h1>
+    <h1>Book Details</h1>
+      <p>Title: {props.title}</p>
+      <p>Author: {props.author}</p>
+      <p>Genre: {props.genre}</p>
+    </div>
+  )
+}
+
+const ArticleInformation = (props) => {
+  return(
+    <div>
+      <h1>Artilce Information</h1>
+      <p>Title: {props.title} C </p>
+      <p>Author: {props.author} </p>
+      <p>Published Data: {props.publishedDate}</p>
+    </div>
+  )
+}
+
+const ProductDetails = (props) => {
+  return(
+    <div>
+      <h1>Product Details</h1>
+      <p>Name: {props.name} </p>
+      <p>Brand: {props.brand} </p>
+      <p>Price: {props.price} </p>
+    </div>
+  )
+}
+
+const EventDetails = (props) => {
+  return(
+    <div>
+      <h1>Event Details</h1>
+      <p>Title: {props.title}</p>
+      <p>Location: {props.location} </p>
+      <p>Date: {props.date}  </p>
+    </div>
+  )
+}
+
+const UserProfile = (props) => {
+  return(
+    <div>
+      <h1>User Profile</h1>
       <p>Name: {props.name}</p>
-      <p>Age: {props.age}</p>
-      <p>Country: {props.country}</p>
+      <img src = {props.url} alt = {props.alt} />
     </div>
   )
 }
+const eventData = {
 
-const WeatherInfo = (props) => {
-  return(
-    <div>
-      <h1>Weather Info</h1>
-      <p>Temperature: {props.temp} C </p>
-      <p>Condition: {props.cond} </p>
-    </div>
-  )
-}
+  title: "Tech Conference 2024",
 
-const UserAvatar = (props) => {
-  return(
-    <div>
-      <h1>UserAvatar</h1>
-      <img src={props.img} alt={props.alt}/>
-    </div>
-  )
-}
+  location: "San Francisco",
 
+  date: "April 15, 2024",
+
+};
+const productData = {
+
+  name: "Smartphone",
+
+  brand: "Samsung",
+
+  price: 799.99,
+
+};
+const bookData = {
+
+  title: "To Kill a Mockingbird",
+
+  author: "Harper Lee",
+
+  genre: "Classic",
+
+};
+const articleData = {
+
+  title: "The Power of Habit",
+
+  author: "Charles Duhigg",
+
+  publishedDate: "March 1, 2024",
+
+};
 function App() {
   return (
     <div>
-       <Greetings name="John"/>
-       <UserDetail name="Bob" age={20}  country="India" />
-       <WeatherInfo temp={30} cond="Sunny" />
-       <UserAvatar img="https://via.placeholder.com/150" alt="SomeImg" />
+       <Greetings name="Emily"/>
+       <BookDetail title={bookData.title} author={bookData.author} genre = {bookData.genre} />
+       <ArticleInformation title={articleData.title} author={articleData.author} publishedDate ={articleData.publishedDate} />
+       <ProductDetails name={productData.name} brand = {productData.brand} price = {productData.price} />
+       <EventDetails title ={eventData.title} location = {eventData.location} date = {eventData.date}  />
+       <UserProfile name="Chirs" url= "https://placehold.co/150x150?text=Hello+World" alt="Unga Bunga"/>
     </div>
   );
 }
