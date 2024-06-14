e1a919d1fef61afd259fda2e5cf6c7f967ca2cf5
Sourav Sarkar <samjhonson932@gmail.com>
2024-06-14 16:37:04 +0530

    IR1.6_HW2

--- CW/test1/src/App.js
+++ CW/test1/src/App.js
@@ -1,61 +1,136 @@
 
 import './App.css';
 
-const GenGreeting = () =>{
-  const greeting = () =>{
-    return "Hello John!"
-  }
-  return <h1>{greeting()}</h1>
+const ReverseArr = () =>{
+    const reverse = (a) =>{
+      return a.reverse()
+    }
+    return (
+      <div>
+        <h1>Reverse Array</h1>
+        <p>Reversed Array: {reverse([1,2,3,4,5]).join(',')}</p>
+      </div>
+    )
 }
 
-const CalculatArea = () =>{
-  const area = (a,b) => a*b
-   return (
+const CalcBmi = () =>{
+  const bmi = (a,b) => a/(b*b)
+  return (
     <div>
-       <p>Rectangle area: {area(10,5)}</p>
-     
-    </div>   
+      <h1>BMI Calculator</h1>
+      <p>BMI: {bmi(70,1.75)} </p>
+    </div>
   )
 }
 
-const CalculatePeri = () =>{
-  const perimter = (a,b) => 2*(a+b)
-  return <p>Rectangle Perimter: {perimter(10,5)}</p>
+const GenQ = () =>{
+  const quotes = [
+
+    "The only way to do great work is to love what you do. - Steve Jobs",
+
+    "Innovation distinguishes between a leader and a follower. - Steve Jobs",
+
+    "Your time is limited, don't waste it living someone else's life. - Steve Jobs",
+
+    "Stay hungry, stay foolish. - Steve Jobs",
+
+    "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
+
+  ];
+  const display = (n) =>{
+    console.log(n)
+    return quotes[n]
+  }
+  const random = Math.floor(Math.random()*quotes.length)
+  return(
+    <div>
+      <h1>Random Quote</h1>
+      <p>{display(random)}</p>
+    </div>
+  )
 }
 
-const GenMessage = () =>{
-  const hrs = new Date().getHours()
-  let greet =""
-  if(hrs<12)
-    {
-      greet="Good Morning!"
-    }
-    else if(hrs>12 && hrs<18)
+const GradeAnalyse = () =>{
+  let remarks = ""
+  let grade=""
+  const Calc = (n) =>{
+    if(n>89)
       {
-        greet="Good Afternoon!"
+        grade = "A"
+        remarks = "Excellent work!"
       }
+    else if(n>79)
+      {
+        grade = "B"
+        remarks = "Good job!"
+      }
+      else if(n>69)
+        {
+          grade = "C"
+          remarks = "Not bad, keep it up!"
+        }
+      else if(n>59)
+        {
+          grade = "D"
+          remarks = "You passed, but aim higher next time."
+        }
+      else
+        {
+          grade = "E"
+         remarks = "Sorry, you didn't pass. Keep working hard!"
+        }
+      return [grade,remarks]
+  }
+  return(
+    <div>
+      <h1>Grade Analyser</h1>
+      <p>Score: 85</p>
+      <p>Grade: {Calc(85)[0]}</p>
+      <p>{Calc(85)[1]}</p>
+    </div>
+  )
+}
+
+const TempFeed = () =>{
+  const result = (n) =>{
+    if(n<0)
+    {
+      return "Brr, it's freezing!"
+    }
+    else if(n<10)
+      {
+        return "It's quite cold, bundle up!"
+      }
+    else if(n<20)
+    {
+      return "The weather is cool and comfortable."
+    }
+    else if(n<30)
+    {
+      return "It's a warm day!"
+    }
     else
     {
-      greet="Good Evening!"
+      return "It's hot outside, stay cool!"
     }
-
-    return <h1>{greet}</h1>
-}
-
-const CalcVolume = () =>{
-  const volume= (a,b) => Math.PI*a*a*b
-  return <p>Volume of Cylinder: {volume(3,5)}</p>
+  }  
+  return(
+    <div>
+      <h1>Temperaure FeedBack</h1>
+      <p>Current Temperature: 25 Degree Celcius</p>
+      <p>{result(25)}</p>
+    </div>
+  )
 }
 
 function App() {
   return (
     <div>
-     <GenGreeting/>
-     <CalculatArea/>
-     <CalculatePeri/>
-     <GenGreeting />
-    <GenMessage />
-    <CalcVolume />
+     <ReverseArr />
+     <CalcBmi />
+     <GenQ />
+     <GradeAnalyse />
+     <TempFeed />
     </div>
   );
 }
