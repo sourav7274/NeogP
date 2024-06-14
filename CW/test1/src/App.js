6f8536ae631020318ea0a7d715f9c9ad9376244a
Sourav Sarkar <samjhonson932@gmail.com>
2024-06-14 14:54:46 +0530

    IR1.6_CW

--- CW/test1/src/App.js
+++ CW/test1/src/App.js
@@ -1,59 +1,44 @@
 
 import './App.css';
 
-const Rnum = () =>{
-  let a = Math.floor(Math.random()*100)+1
-  return <p>Random number between 1 and 100 is {a}</p>
-}
+const MathOPs = () => {
+  const a = 30
+  const b = 20
+  const getSum = (x,y) => {
+    return x+y
+  }
+  const getDiff = (x,y) => {
+    return x-y
+  }
 
-const AbsVal = () =>{
-  const number = -10
-  return <p>Absolute Value: {Math.abs(number)}</p>
-}
-
-const SumE = () => {
-  const numbers = [1, 2, 3, 4, 5, 6, 7]
-  const sum = numbers.filter(n => {
-    let sum = 0
-    if(n%2===0)
-      {
-        sum+=n
-      }
-    return sum
-  })
-  return <p>Sum of Even Numbers: {sum}</p>
-}
-
-const Psq = () => {
-  const side = 5
   return(
     <div>
-      <h1>Square Perimeter Calculator</h1>
-      <p>Side Length: {side}cm</p>
-      <p>Perimeter: {side*4}cm</p>
+      <p>{a} + {b} equals {getSum(a,b)}</p>
+      <p>{a} - {b} equals {getDiff(a,b)}</p>
     </div>
   )
 }
 
-const Convert =() =>{
-  const amount = 50
-  return(
-    <div>
-      <h1>Currency Converter</h1>
-      <p>Amount in USD:${amount} </p>
-      <p>Exchnage Rate: 1USD = 74.5INR </p>
-      <p>Amount in INR: INR {amount*74.5}</p>
-    </div>
-  )
+const CallGreeting = () =>{
+  const greeting = () =>{
+    return "Hello World"
+  }
+  return <h1>{greeting()}</h1>
+}
+
+const DateFomrate = () => {
+  const getCurrentDate = () =>{
+    return new Date().toLocaleDateString()
+  }
+
+  return <h1>{getCurrentDate()}</h1>
 }
 function App() {
   return (
     <div>
-      <Rnum />
-      <AbsVal/>
-      <SumE />
-      <Psq />
-      <Convert />
+     <MathOPs />
+     <CallGreeting/>
+     <DateFomrate/>
     </div>
   );
 }
