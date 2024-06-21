1a683d50f9d013cf49a2089debed19502b1087af
Sourav Sarkar <samjhonson932@gmail.com>
2024-06-21 10:33:25 +0530

    IR1.7_CW

--- CW/test1/src/App.js
+++ CW/test1/src/App.js
@@ -1,124 +1,32 @@
 
 import './App.css';
 
-const ReverseArr = () =>{
-    const reverse = (a) =>{
-      return a.reverse()
-    }
-    return (
-      <div>
-        <h1>Reverse Array</h1>
-        <p>Reversed Array: {reverse([1,2,3,4,5]).join(',')}</p>
-      </div>
-    )
+const Message = ({isLoggin}) => {
+  // if(isLoggin)
+  // {
+  //   return <p>Welcome back,User</p>
+  // }
+  // else{
+  //   return <p>Please Login to continue</p>
+  // }
+
+  return isLoggin ? <p>Welcome Back User!</p> : <p>Please Login to continue</p>
+} 
+
+const Greeting = ({isMorning}) => {
+  return <p>{isMorning ? "Good Morning": "Good Eveinig" }</p> 
 }
 
-const CalcBmi = () =>{
-  const bmi = (a,b) => a/(b*b)
-  return (
-    <div>
-      <h1>BMI Calculator</h1>
-      <p>BMI: {bmi(70,1.75)} </p>
-    </div>
-  )
+const ShowNoti = ({noti}) => {
+  return <div>{noti && <p>{noti}</p>}</div>
 }
 
-const GenQ = () =>{
-  const quotes = [
+const TicketPricing = ({passangerAge}) =>{
+  let price = 100
 
-    "The only way to do great work is to love what you do. - Steve Jobs",
-
-    "Innovation distinguishes between a leader and a follower. - Steve Jobs",
-
-    "Your time is limited, don't waste it living someone else's life. - Steve Jobs",
-
-    "Stay hungry, stay foolish. - Steve Jobs",
-
-    "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
-
-  ];
-  const display = (n) =>{
-    console.log(n)
-    return quotes[n]
-  }
-  const random = Math.floor(Math.random()*quotes.length)
   return(
     <div>
-      <h1>Random Quote</h1>
-      <p>{display(random)}</p>
-    </div>
-  )
-}
-
-const GradeAnalyse = () =>{
-  let remarks = ""
-  let grade=""
-  const Calc = (n) =>{
-    if(n>89)
-      {
-        grade = "A"
-        remarks = "Excellent work!"
-      }
-    else if(n>79)
-      {
-        grade = "B"
-        remarks = "Good job!"
-      }
-      else if(n>69)
-        {
-          grade = "C"
-          remarks = "Not bad, keep it up!"
-        }
-      else if(n>59)
-        {
-          grade = "D"
-          remarks = "You passed, but aim higher next time."
-        }
-      else
-        {
-          grade = "E"
-         remarks = "Sorry, you didn't pass. Keep working hard!"
-        }
-      return [grade,remarks]
-  }
-  return(
-    <div>
-      <h1>Grade Analyser</h1>
-      <p>Score: 85</p>
-      <p>Grade: {Calc(85)[0]}</p>
-      <p>{Calc(85)[1]}</p>
-    </div>
-  )
-}
-
-const TempFeed = () =>{
-  const result = (n) =>{
-    if(n<0)
-    {
-      return "Brr, it's freezing!"
-    }
-    else if(n<10)
-      {
-        return "It's quite cold, bundle up!"
-      }
-    else if(n<20)
-    {
-      return "The weather is cool and comfortable."
-    }
-    else if(n<30)
-    {
-      return "It's a warm day!"
-    }
-    else
-    {
-      return "It's hot outside, stay cool!"
-    }
-  }  
-  return(
-    <div>
-      <h1>Temperaure FeedBack</h1>
-      <p>Current Temperature: 25 Degree Celcius</p>
-      <p>{result(25)}</p>
+      {passangerAge > 60 ? <p>Ticket Price: {price - price*0.15}</p> : <p>Ticket Price: {price}</p>}
     </div>
   )
 }
@@ -126,11 +34,10 @@ const TempFeed = () =>{
 function App() {
   return (
     <div>
-     <ReverseArr />
-     <CalcBmi />
-     <GenQ />
-     <GradeAnalyse />
-     <TempFeed />
+     <Message isLoggin={true} />
+     <Greeting isMorning={true} />
+     <ShowNoti noti="iugeugdfhg" />
+     <TicketPricing passangerAge={50} />
     </div>
   );
 }
