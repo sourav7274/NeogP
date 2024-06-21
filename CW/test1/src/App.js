4e3844eb6c842550a89b9586ef15ba91be78efbb
Sourav Sarkar <samjhonson932@gmail.com>
2024-06-21 11:01:43 +0530

    IR1_Assignment

--- CW/test1/src/App.js
+++ CW/test1/src/App.js
@@ -1,31 +1,25 @@
 
 import './App.css';
 
-const CreditCardPayment = ({bill}) =>{
-  return bill>1000?<p>Please your credit card for payment</p> : <p>You can pay using cash or debit card</p>
-  }
-const ProductAvailability =({inStock}) =>{
-  return inStock? <p>Product is available order now!</p> :<p>Sorry,the product is out of stock</p>
-}
-const Trafficlight = ({color}) =>{
-  return color === "red" ? <p>Stop the lights are red!.</p>:<p>Go! The light is green.</p>
+const ProductPage = ({inStock}) =>{
+  return(
+    <div>
+      <h1>Product Page</h1>
+      <h2>iPhone 15 Pro</h2>
+      <img src="https://placehold.co/150x150?text=Iphone 15"  alt="SomePhone" />
+      <p><b>Price:</b>$499.99</p>
+      <p><b>Color:</b>Midnight Green</p>
+      <p><b>Description:</b>The lastest flagship smartphone from Apple with cutting-edge features amd stunning designs</p>
+      <p><b>{inStock ? "In Stock": "Out of Stock" }</b></p>
+      <p>&copy;2024 Apple Inc.</p>
+    </div>
+  )
 }
 
-const MembershipStatus = ({isPremium}) =>{
-  return isPremium ? <p>Welcome,Premium Memeber</p>:<p>Upgrade to premium for exclusive benefits.</p>
-}
-
-const WeatherReport = ({isSuny}) =>{
-  return isSuny ? <p>Its is a Sunny Day</p> :<p>It is not sunny today.</p>
-}
 function App() {
   return (
     <div> 
-        <CreditCardPayment bill={500} />
-        <ProductAvailability inStock={true} />
-        <Trafficlight color={"htgjhd"} />
-        <MembershipStatus isPremium={false} />
-        <WeatherReport isSuny={true} />
+        <ProductPage inStock={false} />
     </div>
   );
 }
