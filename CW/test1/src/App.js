b8c9a0233e3ebd2fe4f00b778ad33889f1cc8411
Sourav Sarkar <samjhonson932@gmail.com>
2024-06-24 12:32:05 +0530

    IR2.1_CW

--- CW/test1/src/App.js
+++ CW/test1/src/App.js
@@ -1,27 +1,83 @@
 
 import './App.css';
 
-const ProductPage = ({inStock}) =>{
-  return(
-    <div>
-      <h1>Product Page</h1>
-      <h2>iPhone 15 Pro</h2>
-      <img src="https://placehold.co/150x150?text=Iphone 15"  alt="SomePhone" />
-      <p><b>Price:</b>$499.99</p>
-      <p><b>Color:</b>Midnight Green</p>
-      <p><b>Description:</b>The lastest flagship smartphone from Apple with cutting-edge features amd stunning designs</p>
-      <p><b>{inStock ? "In Stock": "Out of Stock" }</b></p>
-      <p>&copy;2024 Apple Inc.</p>
+const ProductList = ({products}) =>{
+  const productListing = products.map(product =>(
+    <div key={product.id}>
+      <h3>Name:{product.name}</h3>
+      <p>Price:{product.price}</p>
     </div>
+  ))
+
+  return(
+    <>
+      <h2>Products</h2>
+      {productListing}
+      <hr/>
+    </>
+  )
+}
+
+const Restaurants = ({restaurants}) =>{
+  const restaurantsList = restaurants.map(restaurant => (
+    <div key={restaurant.id}>
+        <h3>Name:{restaurant.name}</h3>
+        <p>Cuisine:{restaurant.cuisine}</p>
+    </div>
+  ))
+
+  return(
+    <>
+      <h2>Restaurants</h2>
+      {restaurantsList}
+      <hr/>
+    </>
+  )
+}
+
+const Videos = ({videos}) =>{
+  const videoList = videos.map(video => (
+    <div key={video.id}>
+      <h3>Name:{video.name}</h3>
+      <p>Views:{video.views}</p>
+    </div>
+  ))
+
+  return(
+    <>
+      <h2>Videos</h2>
+      {videoList}
+    </>
   )
 }
 
 function App() {
+  
+  const products = [
+    {id:1,name:"Product 1",price:19.99},
+    {id:2,name:"Product 2",price:29.99},
+    {id:3,name:"Product 3",price:39.99},
+  ]
+
+  const restaurants = [
+    {id:1,name:"Restaurant 1",cuisine:"Italian"},
+    {id:2,name:"Restaurant 2",cuisine:"Mexican"},
+    {id:3,name:"Restaurant 3",cuisine:"Chineese"},
+  ]
+
+  const videos = [
+    {id:1,name:"Video 1",views:2000},
+    {id:2,name:"Video 2",views:4000},
+    {id:3,name:"Video 3",views:5000},
+  ]
+
   return (
-    <div> 
-        <ProductPage inStock={false} />
-    </div>
-  );
+    <div>
+         <ProductList products={products} />
+         <Restaurants restaurants={restaurants}/>
+         <Videos videos={videos}/>
+    </div>   
+  )
 }
 
 export default App;
