97fb2e93c23de2de95fdae5c2073963366df1522
Sourav Sarkar <samjhonson932@gmail.com>
2024-08-07 11:41:16 +0530

    IR_Practice7

--- CW/reactlearning/src/App.js
+++ CW/reactlearning/src/App.js
@@ -1,97 +1,75 @@
 import {useState} from 'react'
-const Food = () =>{
-  const questions = [
-    {
-      question: "Which type of cuisine do you prefer?",
-      options: {
-        Asian: ["Chinese", "Japanese", "Indian"],
-        European: ["Italian", "French", "Spanish"],
-        American: ["Burgers", "Pizza", "BBQ"],
-      },
-    },
-  ];
-  const [cuisine,setCuisine] = useState("")
-  const [dish,setDish] = useState([])
+const title = 'React is awesome'
+const content = 'React is a JavaScript library for building user interfaces.'
+
+const heading = 'About Me'
+const name = 'Preeti' // you can put your name
+const learning = 'I am learning React JS currently at neoG Camp.'
+
+const Article = ({title,content}) => {
   const [dis,setDis] = useState(false)
-  const displayCu = Object.keys(questions[0].options).map((c) => (<option value={c}>{c}</option>))
-  const dishHandle = (event) =>{
-      const value = event.target.value
-      if(event.target.checked)
-      {
-        setDish((pval) => [...pval,value])
-      }
-      else
-      {
-        setDish((pval) => pval.filter(p => p!=value))
-      }
-  }
   return(
     <div>
-      <h1>Food Survery App</h1>
-   { dis === false ?  <><label>Select Your Cuisine: </label>
-      <select onChange={(e) => setCuisine(e.target.value)}>
-        <option>-- Select Your Cuisine --</option>
-        {displayCu}
-      </select>
-      <br/>
-      <br/>
-      { cuisine &&
-      <>
-       <label>Select Your Prefered Cuisine</label>
-       {questions[0].options[cuisine].map((c) => (<div>
-        <input onChange={dishHandle} value={c} name='dish' type='checkbox' />{c}
-       </div>))}
-       <br/>
-      </>}
-      <button onClick={() => setDis(true)}>Submit</button></>:
-      <>
-        <h2>Thank You for Sharing Your preferences!</h2>
-        <p>Cusine: {cuisine}</p>
-        <p>Prefered Options: {dish.join(', ')}</p>
-      </>}
+      <h1>{title}</h1>
+      <button onClick={() => setDis(true)}>Know More</button>
+      {dis && <p>{content}</p>}
     </div>
   )
 }
 
-const Feedback =() =>{
-  const [val,setVal] = useState("")
-  const [type,setType] = useState("")
+const About = ({heading,name,learning}) => {
   const [dis,setDis] = useState(false)
   return(
     <div>
-      <h1>Feedback Form</h1>
-      <label>Feedback Type </label>
-      <select onChange={(e) => (setVal(""),setType(e.target.value))}>
-        <option>-- Select Feedback Type --</option>
-        <option value="Compliment">Compliment</option>
-        <option value="Complaint">Complaint</option>
-        <option value="Suggestion">Suggestion</option>
-        <option value="Others">Others</option>
-      </select>
-      <br/>
-      <br/>
-      {type && 
-            <>
-              <label>Comments:</label>
-              <textarea value={val} onChange={(e) => setVal(e.target.value)} rows={5} cols={30}/>
-              <br/>    
-              <br/>
-            </>
-      }
-      <button onClick={() => setDis(true)}>Submit</button>
-      {dis && <>
-        <p>{type}</p>
-        <p>This is a {type}</p>
-      </>}
+      <h1>{heading}</h1>
+      <h2>{name}</h2>
+      <button onClick={() => setDis(true)}>Know More</button>
+      {dis && <p>{learning}</p>}
     </div>
   )
 }
+
+const UserInfo =({username,email,age}) =>{
+  const [dis,setDis] = useState(false)
+  return(
+    <div>
+      <h1>User Information</h1>
+      <p><b>Username: </b>{username}</p>
+      {dis === true 
+        ?<>
+          <p><b>Email: </b>{email}</p>
+          <p><b>Age: </b>{age}</p>
+        </> 
+        :<><button onClick={() => setDis(true)}>See Profile</button></>}
+    </div>
+  )
+}
+
+const UserProfile =({username,imgurl,bio,location}) =>{
+  const [dis,setDis] = useState(false)
+  return(
+    <div>
+      <h1>User Profile</h1>
+      <p><b>Username: </b>{username}</p>
+      {dis === true 
+        ?<>
+        <img src={imgurl}/>
+          <p><b>Bio: </b>{bio}</p>
+          <p><b>Location: </b>{location}</p>
+        </> 
+        :<><button onClick={() => setDis(true)}>View Profile</button></>}
+    </div>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
-      <Food/>
-      <Feedback/>
+      <Article title={title} content={content} />
+      <About heading={heading} learning={learning} name={name}/>
+      <UserInfo username="unga" email="sammy7274@got.com" age={50} />
+      <UserProfile username="bunga" imgurl="https://placehold.co/200x200" bio="Developer" location="Africa" />
     </div>
   );
 }
