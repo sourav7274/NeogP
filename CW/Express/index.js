ed8018c5df232173294bb89a220221f6d7059891
Sourav Sarkar <samjhonson932@gmail.com>
2024-09-25 10:42:14 +0530

    BE3.2_CW

--- CW/Express/index.js
+++ CW/Express/index.js
@@ -2,24 +2,32 @@ const express = require('express')
 require('dotenv').config()
 const app = express();
 
+app.use(express.json())
+
+const cars = [
+    {id:1,make:"Toyota",model:"Camry",year:2022}
+]
+
 app.get('/',(req,res) => {
-    res.send("Hello from Express JS")
+    res.send("Home page")
 })
 
-app.get('/signin',(req,res) =>{
-    res.send("This is Sign in Page")
+app.post('/cars',(req,res) => {
+    const newCar = req.body
+
+    if(!newCar.make || !newCar.model || !newCar.year)
+    {
+        res.status(400).json({error:"Make model and year are needed"})
+    } 
+    else
+    {
+        cars.push(newCar)
+        res.status(201).json({message: "Car added successfully",car:newCar})
+    }
 })
 
-app.get('/booking',(req,res) => {
-    res.send("Book your tickets")
-})
-
-app.get('/clothing/kids',(req,res) =>{
-    res.send("This is the kids wear page")
-})
-
-app.get('/blog',(req,res) => {
-    res.send("This is the blog page")
+app.get('/cars',(req,res) => {
+    res.send(cars)
 })
 
 const PORT = process.env.PORT || 3000;
