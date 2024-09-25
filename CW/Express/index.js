b9ed68639f2fa89f4a4f55f3f9f85afb7f05a55e
Sourav Sarkar <samjhonson932@gmail.com>
2024-09-25 21:38:30 +0530

    BE3.3_CW

--- CW/Express/index.js
+++ CW/Express/index.js
@@ -5,8 +5,23 @@ const app = express();
 app.use(express.json())
 
 const cars = [
-    {id:1,make:"Toyota",model:"Camry",year:2022}
-]
+    { id: 1, make: "Toyota", model: "Camry", year: 2022 },
+    { id: 2, make: "Honda", model: "Civic", year: 2021 },
+    { id: 3, make: "Ford", model: "Mustang", year: 2022 },
+    { id: 4, make: "Chevrolet", model: "Corvette", year: 2023 },
+    { id: 5, make: "Tesla", model: "Model 3", year: 2021 },
+    { id: 6, make: "Nissan", model: "Altima", year: 2022 },
+    { id: 7, make: "BMW", model: "M3", year: 2023 }, 
+    { id: 8, make: "Mercedes-Benz", model: "C-Class", year: 2021 },
+    { id: 9, make: "Audi", model: "A4", year: 2022 },
+    { id: 10, make: "Lexus", model: "RX", year: 2023 },
+    { id: 11, make: "Hyundai", model: "Tucson", year: 2021 },
+    { id: 12, make: "Kia", model: "Seltos", year: 2022 },
+    { id: 13, make: "Mazda", model: "CX-5", year: 2023 },
+    { id: 14, make: "Subaru", model: "Outback", year: 2021 },
+    { id: 15, make: "Volkswagen", model: "Golf", year: 2022 }
+  ];
+  
 const books = [
 
     { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925 },
@@ -123,6 +138,21 @@ app.get('/books',(req,res) =>{
 })
 app.get('/cars',(req,res) => {
     res.send(cars)
+})
+
+
+app.delete('/cars/:id',(req,res) => {
+    const carId = req.params.id
+    const index = cars.findIndex(car => car.id == carId)
+    if(index === -1)
+    {
+        res.status(404).json({error:"Car not Found"})
+    }
+    else
+    {
+        cars.splice(index,1)
+        res.status(200).json({message:"Delteed Successulyy"})
+    }
 })
 
 const PORT = process.env.PORT || 3000;
