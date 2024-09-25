2c9a47b359a3c5574d36d715ad2ae3ef0af1aa6d
Sourav Sarkar <samjhonson932@gmail.com>
2024-09-25 19:11:22 +0530

    BE3.2_HW1

--- CW/Express/index.js
+++ CW/Express/index.js
@@ -7,6 +7,18 @@ app.use(express.json())
 const cars = [
     {id:1,make:"Toyota",model:"Camry",year:2022}
 ]
+const books = [
+
+    { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925 },
+  
+    { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960 }
+  
+  ];
+  const todos = [
+
+    { id: 1, title: 'Water the plants', day: 'Saturday' },
+  
+  ];
 
 app.get('/',(req,res) => {
     res.send("Home page")
@@ -26,6 +38,39 @@ app.post('/cars',(req,res) => {
     }
 })
 
+app.post('/books',(req,res) =>{
+    const newBook =  req.body
+    if(!newBook.title || !newBook.author || !newBook.year)
+    {
+        res.status(400).json({error: "Title author and year are needed"})
+    }
+    else
+    {
+        books.push(newBook)
+        res.status(200).json({message:"Book Added Successfully",book:newBook})
+    }
+})
+
+app.post('/todos',(req,res)=>{
+    const newTodo = req.body
+    if(!newTodo.title || !newTodo.day)
+    {
+        res.status(400).json({error:"Add details"})
+    }
+    else
+    {
+        todos.push(newTodo)
+        res.status(200).json({message:"Addedd Successfully"})
+    }
+})
+
+app.get('/todos',(req,res) =>{
+    res.send(todos)
+})
+
+app.get('/books',(req,res) =>{
+    res.send(books)
+})
 app.get('/cars',(req,res) => {
     res.send(cars)
 })
