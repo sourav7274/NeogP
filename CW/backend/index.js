5730e68cc7c3dee045bf0d7dfa37e39743fdaa27
Sourav Sarkar <samjhonson932@gmail.com>
2024-10-01 18:02:11 +0530

    test vercel

--- CW/backend/index.js
+++ CW/backend/index.js
@@ -521,24 +521,6 @@ async function findByDirec(directorName)
   }
 }
 
-
-
-// const res1= {
-//   name: "Somi",
-//   cuisine: ["Greek"],
-//   location: "11 Main Road, Gem",
-//   rating: 4.3,
-//   reviews: [],
-//   website: "https://somi-example.com",
-//   phoneNumber: "+1234997390",
-//   openHours: "Tue-Sun: 11:00 AM - 10:00 PM",
-//   priceRange: "$$ (11-30)",
-//   reservationsNeeded: false,
-//   isDeliveryAvailable: true,
-//   menuUrl: "https://somi-example.com/menu",
-//   photos: ["https://example.com/somi-photo1.jpg", "https://example.com/somi-photo2.jpg"],
-// };
-
 // async function seedRes(data)
 // {
 //     try{
