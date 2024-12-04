332af7e1a67a806680192670a7895ea9019f3583
Sourav Sarkar <samjhonson932@gmail.com>
2024-12-04 08:47:05 +0530

    fsp1.1

--- CW/backend/index.js
+++ CW/backend/index.js
@@ -26,13 +26,16 @@ const secret_key = "supersecret"
 const JWT_SECRET = "ursecret"
 
 const verifyJWT = (req,res,next) =>{
-  const token = req.headers["Autohorization"]
+  const token = req.headers["authorization"]
   if(!token)
   {
-    return res.status(401).json({message:"No token was provided"})
+    return res.status(401).json({message:"No token provided"})
   }
   try{
-    console.log(token)
+    // console.log(token)
+    const decodedToken = jwt.verify(token,JWT_SECRET)
+    req.user = decodedToken
+    next()
   } catch(error) {
   return res.status(402).json({message:"Invalid Token"})
   }
@@ -56,6 +59,11 @@ app.post('/admin/login',(req,res) =>{
 app.get("/admin/api/data",verifyJWT, (req,res) =>{
   res.json({message:"Protected route accessible"})
 })
+
+
+
+
+
 
 //BE4_Assignment_2
 async function deleteRecipeByID(id)
